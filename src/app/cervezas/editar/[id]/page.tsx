2176077e306ef5b3603eb46e9b24c0050392bd7c
nachodiazcode <ignaciosergiodiaz@gmail.com>
2025-06-12 23:55:03 -0400

    fix : last updates

--- src/app/cervezas/editar/[id]/page.tsx
+++ src/app/cervezas/editar/[id]/page.tsx
@@ -60,7 +60,7 @@ export default function EditarCervezaPage() {
   useEffect(() => {
     const fetchCerveza = async () => {
       try {
-        const res = await axios.get(`${API_URL}/beer/${id}`);
+        const res = await axios.get(`${API_URL}api/beer/${id}`);
         const data = res.data?.datos;
         if (data) {
           setCerveza({
